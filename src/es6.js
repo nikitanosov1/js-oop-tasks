5e2588ff7da7520746e4570a6c6f4f96c6b4c50f
nikitanosov1 <nikinosov2002@mail.ru>
2022-11-18 01:36:52 +0400

    solved fiotoname, filterunique and calculatesalarydifference

--- src/es6.js
+++ src/es6.js
@@ -5,19 +5,32 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName(fio) {}
+function fioToName(fio) {
+    const [fam, name, ot] = new String(fio).split(' ')
+    return name + ' ' + fam;
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique(array) {}
+function filterUnique(array) {
+    return Array.from(new Set(array));
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference(array) {}
+function calculateSalaryDifference(array = []) {
+    if (array.length === 0) return false;
+    const result = array.reduce((accumulator, currentValue) => {
+        accumulator.max = Math.max(accumulator.max, currentValue);
+        accumulator.min = Math.min(accumulator.min, currentValue);
+        return accumulator;
+    }, {min: array.at(0), max: array.at(0)})
+    return result.max / result.min;
+}
 
 // Реализуйте класс "словарь слов" (как толковый словарь)
 // класс должен быть безопасным и работать только со словами
